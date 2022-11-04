6fcb1329340d99a79699ca7074f13e2cad4d3003
Kingminer7 <100500023+Kingminer7@users.noreply.github.com>
2022-11-04 14:53:07 -0400

    1 line

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+const fs = require("fs")
+const express = require("express")
 
+const app = express()
+
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+  console.log(req.hostname)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
